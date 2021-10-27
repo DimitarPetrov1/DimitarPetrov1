79068ab84b2e90dd4e9fd2ea735ec549868a301a
Dimitar Petrov <60468978+DimitarPetrov1@users.noreply.github.com>
2021-10-27 22:01:13 +0300

    clock link

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ const projects = [
     cat: "Javascript",
     name: "Weather forecast",
     img: "./images/weather.jpg",
-    link: "https://digital-clock001.netlify.app",
+    link: "https://weather-forecaster-app1.netlify.app",
   },
     {
     cat: "Javascript",
     name: "Digital clock",
     img: "./images/clock.jpg",
-    link: "https://weather-forecaster-app1.netlify.app",
+    link: "https://digital-clock001.netlify.app",
   },
   {
     cat: "ReactJS",
